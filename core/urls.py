36726de42b996d0a7ceaf0b9fbb75a5131da6fd3
username <email>
2024-09-07 21:51:46 +0500

    signup enpoint

--- core/urls.py
+++ core/urls.py
@@ -3,7 +3,9 @@ from django.urls import path, re_path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-from apis.views import SignIn
+from apis.views import (
+   SignIn, SignUp
+)
 
 
 schema_view = get_schema_view(
@@ -20,6 +22,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('signin/', SignIn.as_view()),
+    path('create/user/',SignUp.as_view()),
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
