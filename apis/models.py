95f45cfffa63060aee104132ce8d8164031fd568
username <email>
2024-09-07 22:12:36 +0500

    message model

--- apis/models.py
+++ apis/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class Message(models.Model):
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sender')
+    recipient = models.ForeignKey(User, on_delete=models.CASCADE, related_name='recipient')
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f'Message from {self.sender} to {self.recipient}'
+        
