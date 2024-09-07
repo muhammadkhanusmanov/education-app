c5d245cfa3df25b006743869447e8cc5914208d1
username <email>
2024-09-07 22:23:36 +0500

    serializers

---
+++ apis/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+from .models import (
+    Message
+)
+class UserSerializer(serializers.ModelSerializer):
+    status = serializers.CharField(source='last_name')
+    full_name = serializers.CharField(source='first_name')
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'full_name', 'status']
+        read_only_fields = ['id']
+
+
+class MessageSerializer(serializers.ModelSerializer):
+    sender = UserSerializer(read_only=True)
+    recipient = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Message
+        fields = ['id', 'sender', 'recipient', 'content', 'created_at']
+        read_only_fields = ['id', 'created_at']
