1a56b4f923154a0ee3c6a6477fc43302d3741302
username <email>
2024-09-11 21:00:52 +0500

    survey model

--- apis/models.py
+++ apis/models.py
@@ -10,3 +10,12 @@ class Message(models.Model):
     def __str__(self):
         return f'Message from {self.sender} to {self.recipient}'
         
+class Survey(models.Model):
+    name = models.CharField(max_length=50)
+    students = models.ManyToManyField(User, related_name='survey')
+    teacher = models.ForeignKey(User, on_delete=models.CASCADE, related_name='survey_teacher')
+    untill_at = models.CharField(max_length=45)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f'{self.name} ({created_at} - {untill_at})'
