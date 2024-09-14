9e072096089a1e8c3aea969fc080fd0362c76c25
username <email>
2024-09-14 15:55:21 +0500

    register

--- apis/admin.py
+++ apis/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from .models import Message, Survey, Vote
+from .models import Message, Survey, Vote,Lesson,Task
 
-admin.site.register([Message,Survey,Vote])
+admin.site.register([Message,Survey,Vote,Lesson,Task])
